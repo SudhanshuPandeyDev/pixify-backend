1e6bfcd9644f2f922d446cdf89a581313c3fe8d6
Sudhanshu Pandey <154343364+SudhanshuPandeyDev@users.noreply.github.com>
2024-12-21 17:56:58 +0530

    Update verifyToken.js

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-// middleware has req,res,next
 const verifyToken = async (req, res, next) => {
   const authHeader = req.header("Authorization");
   const token = authHeader && authHeader.split(" ")[1];
